7629bc73d330c588e2b4e1b302f172a3dc571ad0
milkaotieno <milka.otieno@student.moringaschool.com>
2022-03-01 13:10:12 +0300

    add images

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,16 +19,11 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 
 
 public class App {
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567;
+
 //    }
     public static void main(String[] args) {
 //        port(getHerokuAssignedPort());
-        //staticFileLocation("/public");
+        staticFileLocation("/public");
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
